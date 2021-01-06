7aecdee84f5b5eda38ba279b9ac88f52d394594a
moliangle <molianglcl@gmail.com>
2021-01-06 19:24:51 +0800

    修改友情码

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -53,7 +53,8 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [`P04z54XCjVWnYaS5u2ak7ZCdan1Bdd2GGiWvC6_uERj`, 'P04z54XCjVWnYaS5m9cZ2ariXVJwHf0bgkG7Uo'];
+const inviteCodes = [`T012_7x1SR0R81LQCjVWnYaS5kRrbA`, 'T0225KkcRE1P9FCFcxv8l_YNfQCjVWnYaS5kRrbA','T016v_1yRB0e81XfIXWpCjVWnYaS5kRrbA','T015uPpwSR8R8VPQT0cCjVWnYaS5kRrbA','T0225KkcRB0Q812FJ06nkqQPcACjVWnYaS5kRrbA'];
+
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
